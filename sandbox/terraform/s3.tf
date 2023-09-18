9216fd8098abbb7043f0ed47e2aaa75e39bf09a0
Andrei Titerlea <andrei.titerlea@liatrio.com>
2023-09-17 19:31:28 -0700

    Renamed file

--- sandbox/terraform/s3.tf
+++ sandbox/terraform/s3.tf
@@ -4,7 +4,3 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
-
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
