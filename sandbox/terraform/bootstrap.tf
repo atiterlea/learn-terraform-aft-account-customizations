e320dbcdf8ad620a9a5631411dcb04524a8e589b
Andrei Titerlea <andrei.titerlea@liatrio.com>
2023-09-17 19:10:58 -0700

    Bootstrapping sandbox

--- sandbox/terraform/bootstrap.tf
+++ sandbox/terraform/bootstrap.tf
@@ -4,3 +4,7 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
